4d402b790ba9d038f7d40aac264ec2d7dbe01a1d
Trachifab <fabiendorey@gmail.com>
2017-07-10 16:31:04 +0200

    Init + models + some starting components

---
+++ frontEnd/src/app/components/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: [
+    '../../../../node_modules/bulma/css/bulma.css'
+  ]
+})
+export class HomeComponent {
+
+}
---
+++ frontEnd/src/app/services/photoService.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Photo } from '../models/photo';
+import { Http, Headers } from '@angular/http';
+import { User } from '../models/user';
+
+@Injectable()
+export class PhotoService{
+
+    constructor (private http: Http){}
+
+    getPhotos() {
+        let url = "http://localhotst:8080/photo/allPhotos";
+        return this.http.get(url);
+    }
+
+
+}
